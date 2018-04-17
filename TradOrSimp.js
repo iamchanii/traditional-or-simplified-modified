9d3332fe2d259419b2739ae77a7c910fcbec0b6b
이찬희 <chanhee.lee@towncompany.co.kr>
2018-04-17 10:21:16 +0900

    v2.0.2: babeled

--- TradOrSimp.js
+++ TradOrSimp.js
@@ -1,61 +1,61 @@
 var characters = require('./characters');
 
-function isTraditional(str){
-  if(detect(str).detectedCharacters == 'traditional'){
+function isTraditional(str) {
+  if (detect(str).detectedCharacters == 'traditional') {
     return true;
-  }else{
+  } else {
     return false;
   }
 }
 
-function isSimplified(str){
-  if(detect(str).detectedCharacters == 'simplified'){
-    return true
-  }else{
-    return false
+function isSimplified(str) {
+  if (detect(str).detectedCharacters == 'simplified') {
+    return true;
+  } else {
+    return false;
   }
 }
 
-function detect(str){
+function detect(str) {
   var result = {
     inputLength: str.length,
     simplifiedCharacters: 0,
     traditionalCharacters: 0
-  }
+  };
 
-  for(var i = 0; i < str.length; i++){
-    var char = str.substr(i, 1)
+  for (var i = 0; i < str.length; i++) {
+    var char = str.substr(i, 1);
     //get both
-    if(charIsS(char)){
-      result.simplifiedCharacters++
+    if (charIsS(char)) {
+      result.simplifiedCharacters++;
     }
 
-    if(charIsT(char)){
-      result.traditionalCharacters++
+    if (charIsT(char)) {
+      result.traditionalCharacters++;
     }
 
-    if(i == str.length -1){
-      if(result.simplifiedCharacters > result.traditionalCharacters){
-        result.detectedCharacters = 'simplified'
+    if (i == str.length - 1) {
+      if (result.simplifiedCharacters > result.traditionalCharacters) {
+        result.detectedCharacters = 'simplified';
         result.detectionRate = result.simplifiedCharacters / result.traditionalCharacters | 1;
-      }else if(result.simplifiedCharacters < result.traditionalCharacters){
-        result.detectedCharacters = 'traditional'
+      } else if (result.simplifiedCharacters < result.traditionalCharacters) {
+        result.detectedCharacters = 'traditional';
         result.detectionRate = result.traditionalCharacters / result.simplifiedCharacters | 1;
-      }else{
-        result.detectedCharacters = 'unknown'
+      } else {
+        result.detectedCharacters = 'unknown';
         result.detectionRate = 0;
       }
-      return result
+      return result;
     }
   }
 }
 
-function charIsS(char){
+function charIsS(char) {
   var escapedChar = escapeRegExp(char);
   return new RegExp(escapedChar).test(characters.simplified);
 }
 
-function charIsT(char){
+function charIsT(char) {
   var escapedChar = escapeRegExp(char);
   return new RegExp(escapedChar).test(characters.traditional);
 }
@@ -72,7 +72,7 @@ function extract(str, target) {
   };
 
   for (var i = 0; i < str.length; i++) {
-    var char = str.substr(i, 1)
+    var char = str.substr(i, 1);
 
     if ((target === 'all' || target === 'simplified') && charIsS(char)) {
       result.simplified.push(char);
@@ -99,10 +99,10 @@ function extractTraditional(str) {
 }
 
 module.exports = {
-  isTraditional,
-  isSimplified,
-  detect,
-  extractAll,
-  extractTraditional,
-  extractSimplified,
-}
+  isTraditional: isTraditional,
+  isSimplified: isSimplified,
+  detect: detect,
+  extractAll: extractAll,
+  extractTraditional: extractTraditional,
+  extractSimplified: extractSimplified
+};
